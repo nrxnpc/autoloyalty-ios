51d38ddf46e5a40c8a56cb39d472cba943a5f6eb
ys56kii <hidden@mail.na>
2025-10-18 17:28:05 +0300

    [Hotfix] Fixed runtime warnings

--- Source/Application/Main/MainView.swift
+++ Source/Application/Main/MainView.swift
@@ -13,19 +13,21 @@ struct MainView: View {
     // MARK: -
     
     var body: some View {
-        NavigationView {
-            switch application.state {
-            case .loading: makeLoading()
-            case .authentication: makeLogin()
-            case .session(let sessionID): makeCustomerSession(for: sessionID)
-            case .guestSession: makeGuestSession()
+        NavigationStack {
+            ZStack {
+                switch application.state {
+                case .loading: makeLoading()
+                case .authentication: makeLogin()
+                case .session(let sessionID): makeCustomerSession(for: sessionID)
+                case .guestSession: makeGuestSession()
+                }
             }
+            .animation(.smooth, value: application.state)
+            .sensoryFeedback(.start, trigger: application.state)
+            .modifier(Main.DestinationProcessor(destination: $router.destination, sheet: $router.sheet))
+            .environmentObject(router)
+            .environmentObject(application)
         }
-        .animation(.smooth, value: application.state)
-        .sensoryFeedback(.start, trigger: application.state)
-        .modifier(Main.DestinationProcessor(destination: $router.destination, sheet: $router.sheet))
-        .environmentObject(router)
-        .environmentObject(application)
         .onShake {
             router.route(sheet: .console)
         }
